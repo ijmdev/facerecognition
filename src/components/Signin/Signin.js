85cd3a7766c63da0f6b44c594bb2325fa8dcc421
Isabeau Dekker <isabeaudekker@Isabeaus-MacBook-Pro.local>
2021-01-01 15:50:38 +0100

    text recovery

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -73,7 +73,7 @@ class Signin extends React.Component {
                 />
             </div>
             <div className="lh-copy mt3">
-                <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Registeer</p>
+                <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
             </div>
         </main>
